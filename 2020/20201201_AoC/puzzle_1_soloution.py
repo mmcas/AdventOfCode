68ab3382f92ce1d4eeffce409c830d0da9c641d0
Madelene <madelene.casselbrant@gmail.com>
2020-12-02 17:50:22 +0100

    Second soloution day 1

--- 2020/20201201_AoC/puzzle_1_soloution.py
+++ 2020/20201201_AoC/puzzle_1_soloution.py
@@ -1,16 +1,26 @@
 from puzzle_1_input import input 
 numbers_list = input()
 
-for num in numbers_list:
-    numbers_list.remove(num)
-    for other_num in numbers_list:
-        if other_num + num == 2020:
-            print(num)
-            print(other_num)
-            print(other_num * num)
-            break
-        else:
-            next
+def find_two_nums(num_list):
+    for num in num_list:
+        num_list.remove(num)
+        for other_num in num_list:
+            if other_num + num == 2020:
+                return(other_num * num)
+            else:
+                next
 
 
-    
+def find_three_nums(num_list):
+    for num in num_list:
+        num_list.remove(num)
+        for other_num in num_list:
+            for third_num in num_list:
+                if third_num + other_num + num == 2020:
+                    return(third_num * other_num * num)
+                else:
+                    next
+
+
+print(find_two_nums(numbers_list))
+print(find_three_nums(numbers_list))
